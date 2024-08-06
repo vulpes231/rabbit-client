027b7e2cda12de9ae9835ebc168a41e22fa8c6b9
Vulpes231 <whykayshot@gmail.com>
2024-08-06 14:31:17 +0100

    bull

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+
+const initialState = {
+  msg: "",
+};
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const [chatMessages, setChatMessages] = useState([]);
+  const [form, setForm] = useState(initialState);
+
+  const sendChatMessage = (e) => {
+    e.preventDefault();
+    if (form.msg.trim() === "") return; // Avoid sending empty messages
+    setChatMessages((prevMessages) => [...prevMessages, form.msg]);
+    setForm(initialState);
+  };
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div>
+      <form onSubmit={sendChatMessage}>
+        {/* Chat messages */}
+        <ul>
+          {chatMessages.map((msg, index) => (
+            <li key={index}>{msg}</li>
+          ))}
+        </ul>
+        <div>
+          <label htmlFor="msg">Enter chat</label>
+          <textarea
+            id="msg"
+            cols={30}
+            rows={5}
+            onChange={handleInput}
+            value={form.msg}
+            name="msg"
+          />
+        </div>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 };
 
 export default Chat;
