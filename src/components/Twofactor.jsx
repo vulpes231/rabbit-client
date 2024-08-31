f4707d4c11940779fd391f9a69f4f909aee21d36
Vulpes231 <whykayshot@gmail.com>
2024-08-31 12:47:28 +0100

    done

--- src/components/Twofactor.jsx
+++ src/components/Twofactor.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { GrShieldSecurity } from "react-icons/gr";
-import { Link, useNavigate } from "react-router-dom";
-import { buyProduct } from "../features/orderSlice";
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-// Link
+import { buyProduct } from "../features/orderSlice";
 
 const features = [
   "multiple layout and pages",
@@ -15,52 +13,16 @@ const features = [
   "100% uptime",
 ];
 
-const Twofactor = ({ title, monthlyPrice, dailyPrice, img }) => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const [success, setSuccess] = useState(false);
-  const [error, setError] = useState(null);
-
-  const { placeOrderError, placeOrderSuccess, placeOrderPending } = useSelector(
-    (state) => state.order
-  );
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = {
-      item: title,
-      price: monthlyPrice,
-    };
-
-    // console.log(data);
-    dispatch(buyProduct(data));
-  };
-
-  useEffect(() => {
-    if (placeOrderSuccess) {
-      setSuccess(true);
-    }
-  }, [placeOrderSuccess]);
-
-  useEffect(() => {
-    if (placeOrderError) {
-      setError(placeOrderError);
-    }
-  }, [placeOrderError]);
-
-  useEffect(() => {
-    let timeout;
-    if (success) {
-      timeout = 6000;
-      setTimeout(() => {
-        setSuccess(false);
-        navigate("/order");
-      }, timeout);
-    }
-    return () => clearTimeout(timeout);
-  }, [success]);
-
+const Twofactor = ({
+  handleSubmit,
+  title,
+  monthlyPrice,
+  dailyPrice,
+  img,
+  error,
+  success,
+  loading,
+}) => {
   return (
     <div className="border border-none dark:border-slate-200 flex flex-col gap-4 p-6 bg-white dark:bg-slate-950 mt-5 shadow-xl rounded-lg">
       <div className="flex gap-3 items-center">
@@ -68,22 +30,20 @@ const Twofactor = ({ title, monthlyPrice, dailyPrice, img }) => {
         <h3 className="uppercase font-bold lg:text-md">{title}</h3>
       </div>
       <small className="text-sm text-slate-800 dark:text-slate-200">
-        2fa links comes with these features:
+        2fa links come with these features:
       </small>
       <ul className="grid lg:grid-cols-2 gap-x-5 list-disc pl-2">
-        {features.map((ft, index) => {
-          return (
-            <li
-              className="capitalize p-1 text-slate-950 dark:text-slate-200 text-xs font-thin"
-              key={index}
-            >
-              {ft}
-            </li>
-          );
-        })}
+        {features.map((ft, index) => (
+          <li
+            className="capitalize p-1 text-slate-950 dark:text-slate-200 text-xs font-thin"
+            key={index}
+          >
+            {ft}
+          </li>
+        ))}
       </ul>
       <div className="text-sm text-slate-800 dark:text-slate-200">
-        <h5>price:</h5>
+        <h5>Price:</h5>
         <span className="flex items-center gap-6 ">
           <small>
             <span className="font-bold lg:text-lg">${monthlyPrice}</span>/month
@@ -94,20 +54,23 @@ const Twofactor = ({ title, monthlyPrice, dailyPrice, img }) => {
         </span>
       </div>
       <div className="flex flex-col gap-1 text-sm text-slate-800 dark:text-slate-200">
-        <p> Link to video demo below</p>
-        <Link className="underline" to={"https://t.me/rabbit2fa/64"}>
-          watch video
-        </Link>
+        <p>Link to video demo below</p>
+        <a
+          className="underline text-red-500"
+          href="https://t.me/rabbit2fa/64"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Watch video
+        </a>
       </div>
       {error && <p className="text-red-500">{error}</p>}
-      {success && (
-        <p className="text-green-500">{"order created successfully"}</p>
-      )}
+      {success && <p className="text-green-500">Order created successfully</p>}
       <button
         onClick={handleSubmit}
         className="border-none bg-red-500 text-white rounded-3xl py-2.5 mt-4 capitalize"
       >
-        {!placeOrderPending ? "order" : "wait..."}
+        {!loading ? "Order" : "Wait..."}
       </button>
     </div>
   );
--- src/pages/Bypass.jsx
+++ src/pages/Bypass.jsx
@@ -1,50 +1,134 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getAccessToken } from "../constants";
 import TabContainer from "./TabContainer";
 import Twofactor from "../components/Twofactor";
 import { email, gg, soft, yh } from "../assets";
+import { useDispatch, useSelector } from "react-redux";
+import { buyProduct, resetPlaceOrder } from "../features/orderSlice";
+
+const myProducts = [
+  {
+    id: 0,
+    title: "office365 2fa link",
+    monthlyPrice: 400,
+    dailyPrice: 300,
+    img: soft,
+  },
+  {
+    id: 1,
+    title: "gmail 2fa link",
+    monthlyPrice: 400,
+    dailyPrice: 300,
+    img: gg,
+  },
+  {
+    id: 2,
+    title: "yahoo 2fa link",
+    monthlyPrice: 400,
+    dailyPrice: 300,
+    img: yh,
+  },
+  {
+    id: 3,
+    title: "custom 2fa link",
+    monthlyPrice: 400,
+    dailyPrice: 300,
+    img: email,
+  },
+];
 
 const Bypass = () => {
-  const navigate = useNavigate();
   const accessToken = getAccessToken();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(null);
+  const [activeProductId, setActiveProductId] = useState(null);
+
+  const { placeOrderError, placeOrderSuccess, placeOrderPending } = useSelector(
+    (state) => state.order
+  );
+
+  const handleSubmit = (e, tool) => {
+    e.preventDefault();
+    setActiveProductId(tool.id); // Set the active product ID
+    const data = {
+      item: tool.title,
+      price: tool.monthlyPrice,
+    };
+
+    dispatch(buyProduct(data));
+  };
+
+  const twoFaTools = myProducts.map((tool) => {
+    const isActive = activeProductId === tool.id;
+    return (
+      <Twofactor
+        key={tool.id}
+        title={tool.title}
+        monthlyPrice={tool.monthlyPrice}
+        dailyPrice={tool.dailyPrice}
+        img={tool.img}
+        error={isActive ? error : null}
+        success={isActive ? success : null}
+        loading={isActive ? placeOrderPending : null}
+        handleSubmit={(e) => handleSubmit(e, tool)}
+      />
+    );
+  });
+
+  useEffect(() => {
+    if (placeOrderSuccess) {
+      setSuccess(true);
+    }
+  }, [placeOrderSuccess]);
+
+  useEffect(() => {
+    if (placeOrderError) {
+      setError(placeOrderError);
+    }
+  }, [placeOrderError]);
+
+  useEffect(() => {
+    let timeout;
+    if (success) {
+      timeout = 6000;
+      setTimeout(() => {
+        setSuccess(false);
+        dispatch(resetPlaceOrder());
+        navigate("/order");
+      }, timeout);
+    }
+    return () => clearTimeout(timeout);
+  }, [success, navigate]);
+
+  useEffect(() => {
+    let timeout;
+    if (error) {
+      timeout = 6000;
+      setTimeout(() => {
+        setError(false);
+        dispatch(resetPlaceOrder());
+      }, timeout);
+    }
+    return () => clearTimeout(timeout);
+  }, [error, dispatch]);
 
   useEffect(() => {
     if (!accessToken) {
       navigate("/signin");
     }
-  }, [accessToken]);
+  }, [accessToken, navigate]);
 
   return (
     <TabContainer>
       <h3 className="text-xl lg:text-2xl font-semibold mt-5 capitalize">
-        2FA bypass
+        2FA Bypass
       </h3>
       <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-8">
-        <Twofactor
-          title={"office365 2fa link"}
-          monthlyPrice={400}
-          dailyPrice={300}
-          img={soft}
-        />
-        <Twofactor
-          title={"gmail 2fa link"}
-          monthlyPrice={400}
-          dailyPrice={300}
-          img={gg}
-        />
-        <Twofactor
-          title={"yahoo 2fa link"}
-          monthlyPrice={400}
-          dailyPrice={300}
-          img={yh}
-        />
-        <Twofactor
-          title={"custom 2fa link"}
-          monthlyPrice={400}
-          dailyPrice={300}
-          img={email}
-        />
+        {twoFaTools}
       </div>
     </TabContainer>
   );
